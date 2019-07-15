b2b5f3c227f29fc15cdb9f94dfe595d0cbefea2d
mirenguerra <miren.guerra.sanz@gmail.com>
2019-07-15 11:37:33 +0200

    Add some bootstrap styles

--- index.js
+++ index.js
@@ -5,65 +5,155 @@ const recipeURL =
 
 const recipeTitle = document.querySelector(".header__title");
 const articlesList = document.querySelector(".articles__list");
+const totalItemsEl = document.querySelector(".buy__items-quantity");
 const subtotal = document.querySelector(".buy__subtotal-quantity");
-const shippingPrice = document.querySelector(".buy__shipping-quantity");
+const shippingPriceEl = document.querySelector(".buy__shipping-quantity");
 const total = document.querySelector(".buy__total-quantity");
 const btnAllSelected = document.querySelector(".select");
 const btnAllUnselected = document.querySelector(".unselect");
 const btnTotal = document.querySelector(".buy-btn");
 
-let items;
-let subtotalPrice = 0;
-let totalPrice = 0;
+let ingredients;
+let totalItems;
+let shippingCost;
+let subtotalPrice;
+let totalPrice;
+let arrCheckbox = [];
 
 function getRecipe() {
   fetch(recipeURL)
     .then(res => res.json())
     .then(data => {
       const recipe = data.recipe;
-      console.log(recipe);
+      ingredients = data.recipe.ingredients;
+
       printRecipe(recipe);
+      getShippingPrice(recipe);
     });
 }
 getRecipe();
 
+function getShippingPrice(recipe) {
+  shippingCost = recipe["shipping-cost"];
+  shippingPriceEl.innerHTML = shippingCost + " €";
+}
+
+function calculateValues() {
+  totalItemsEl.innerHTML = totalItems;
+  subtotal.innerHTML = `${totalPrice.toFixed(2)} €`;
+  total.innerHTML =
+    totalItems !== 0
+      ? `${(totalPrice + shippingCost).toFixed(2)} €`
+      : `${totalPrice.toFixed(2)} €`;
+  btnTotal.innerHTML = total.innerHTML;
+}
+
+function handleInputCheckbox(event) {
+  const selectedCheckbox = event.target.checked;
+  const id = event.target.id;
+  if (selectedCheckbox) {
+    ingredients[id].items = 1;
+    totalItems[id].value = ingredients[id].items;
+  } else {
+    ingredients[id].items = 0;
+    totalItems[id].value = ingredients[id].items;
+  }
+
+  calculateValues();
+}
+
+function handleInputQuantity(event) {
+  const value = event.target.value;
+  const id = event.target.id;
+  if (value === "") {
+    ingredients[id].items = 0;
+    arrCheckbox[id].checked = false;
+  } else {
+    ingredients[id].items = value;
+    arrCheckbox[id].checked = true;
+  }
+  calculateValues();
+}
+
 function printRecipe(recipe) {
   recipeTitle.innerHTML = recipe.name;
-  shippingPrice.innerHTML = recipe["shipping-cost"];
 
-  recipe.ingredients.map(ingredient => {
-    const newItemEl = document.createElement("div");
+  ingredients.map((ingredient, index) => {
+    const newItemEl = document.createElement("li");
+    newItemEl.id = index;
+    newItemEl.classList.add(
+      "articles__item",
+      "listItem",
+      "list-group-item",
+      "d-flex",
+      "align-items-center",
+      "row",
+      "p-1"
+    );
 
     const newInputEl = document.createElement("input");
     newInputEl.type = "checkbox";
     newInputEl.name = "ingredients";
     newInputEl.value = ingredient.price;
-    newInputEl.classList.add("articles__item-checkbox");
+    newInputEl.id = index;
+    newInputEl.classList.add(
+      "articles__item-checkbox",
+      "col-1",
+      "d-flex",
+      "justify-content-center"
+    );
+    arrCheckbox = document.querySelectorAll("articles__item-checkbox");
+    newInputEl.addEventListener("click", handleInputCheckbox);
 
     const newItemQuantityEl = document.createElement("input");
     newItemQuantityEl.type = "number";
     newItemQuantityEl.name = "itemsQuantity";
-    newItemQuantityEl.value = 1;
+    newItemQuantityEl.value = ingredient.items;
+    newItemQuantityEl.min = 0;
+    newItemQuantityEl.id = index;
+    newItemQuantityEl.addEventListener("change", handleInputQuantity);
+    newItemQuantityEl.classList.add(
+      "counter-data",
+      "d-flex",
+      "text-center",
+      "col-1",
+      "border",
+      "p-0"
+    );
+    const ingredientDataContainer = document.createElement("div");
+    ingredientDataContainer.classList.add("ingredient-data-container");
+    ingredientDataContainer.classList.add("col-8");
 
     const newProductEl = document.createElement("h1");
     const newProduct = document.createTextNode(ingredient.product);
+    newProductEl.classList.add("m-0", "font-weight-bold");
     newProductEl.appendChild(newProduct);
 
     const newBrandEl = document.createElement("h2");
     const newBrand = document.createTextNode(ingredient.brand);
+    newBrandEl.classList.add("m-0", "text-secondary", "small");
     newBrandEl.appendChild(newBrand);
 
-    const newQuantityEl = document.createElement("p");
-    const newQuantity = document.createTextNode(ingredient.price + " €");
+    const newQuantityEl = document.createElement("h2");
+    const newQuantity = document.createTextNode(ingredient.quantity);
+    newQuantityEl.classList.add("m-0", "small");
     newQuantityEl.appendChild(newQuantity);
+
+    const newPriceEl = document.createElement("p");
+    const newPrice = document.createTextNode(ingredient.price + " €");
+    newPriceEl.classList.add("price-item", "text-success", "font-weight-bold");
+    newPriceEl.appendChild(newPrice);
+
+    ingredientDataContainer.appendChild(newProductEl);
+    if (ingredient.brand !== undefined) {
+      ingredientDataContainer.appendChild(newBrandEl);
+    }
+    ingredientDataContainer.appendChild(newQuantityEl);
 
     newItemEl.appendChild(newInputEl);
     newItemEl.appendChild(newItemQuantityEl);
-    newItemEl.appendChild(newProductEl);
-    if (ingredient.brand !== undefined) {
-      newItemEl.appendChild(newBrandEl);
-    }
-    newItemEl.appendChild(newQuantityEl);
+    newItemEl.appendChild(ingredientDataContainer);
+    newItemEl.appendChild(newPriceEl);
 
     articlesList.appendChild(newItemEl);
   });
